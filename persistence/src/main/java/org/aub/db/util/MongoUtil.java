1267c7075fc2c2e5b737266d1fcabb683a7d7422
KPACHblu <aubolut@gmail.com>
2014-10-11 22:09:30 +0300

    Add Openshift related code for mongo DB

--- persistence/src/main/java/org/aub/db/util/MongoUtil.java
+++ persistence/src/main/java/org/aub/db/util/MongoUtil.java
@@ -1,15 +1,13 @@
 package org.aub.db.util;
 
-import com.mongodb.DB;
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoException;
+import com.mongodb.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 
 @ApplicationScoped
 public class MongoUtil {
@@ -24,7 +22,7 @@ public class MongoUtil {
     @PostConstruct
     private void init() {
         try {
-            MongoClient mongoClient = new MongoClient(HOST, PORT);
+            MongoClient mongoClient = (isLocalEnvironment())? getLocalClient() : getOpenshiftClient();
             db = mongoClient.getDB(DB_NAME);
         } catch (UnknownHostException | MongoException e) {
             logger.error(e.getMessage());
@@ -34,4 +32,31 @@ public class MongoUtil {
     public DB getDb() {
         return db;
     }
+
+    private boolean isLocalEnvironment() {
+        boolean isLocalEnvironment = true;
+        if (System.getenv("OPENSHIFT_MONGODB_DB_HOST") != null) {
+            isLocalEnvironment = false;
+        }
+        return  isLocalEnvironment;
+    }
+
+    private MongoClient getLocalClient() throws UnknownHostException {
+        return new MongoClient(HOST, PORT);
+    }
+
+    private MongoClient getOpenshiftClient() throws UnknownHostException {
+        String host = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
+        String portRaw = System.getenv("OPENSHIFT_MONGODB_DB_PORT");
+        int port = Integer.decode(portRaw);
+        String db = System.getenv("OPENSHIFT_APP_NAME");
+        String user = System.getenv("OPENSHIFT_MONGODB_DB_USERNAME");
+        String password = System.getenv("OPENSHIFT_MONGODB_DB_PASSWORD");
+
+        MongoCredential credential = MongoCredential.createMongoCRCredential(user, db, password.toCharArray());
+
+        return new MongoClient(new ServerAddress(host, port), Arrays.asList(credential));
+    }
+
+
 }
